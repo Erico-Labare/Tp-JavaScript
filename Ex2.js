bbc8b6e8be1df15372bf638970b4012bc7697b0e
Erico Labare <erico.labare@outlook.com>
2024-12-10 11:36:29 +0100

    Ex2 + misc

--- Ex2.js
+++ Ex2.js
@@ -4,7 +4,7 @@ const prompt = require("prompt-sync")({ sigint: true });
 const askAge = () => {
     const age = parseInt(prompt("Entrez votre âge : "));
 
-    if (isNaN(age) || age < 0) {
+    if (isNaN(age) || age <= 0) {
         console.log("Erreur : veuillez entrer un âge valide (un nombre positif).");
         return askAge();
     }
