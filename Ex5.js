2f108e7dc6ea5b6fe98b9f465a6bc0143bb4e55f
Erico Labare <erico.labare@outlook.com>
2024-12-10 12:00:32 +0100

    Ex5

---
+++ Ex5.js
@@ -0,0 +1,13 @@
+let variable1;
+if (variable1 === undefined) {
+    console.log("La variable1 est undefined.");
+} else {
+    console.log("La variable1 n'est pas undefined.");
+}
+
+let variable2 = null;
+if (variable2 === null) {
+    console.log("La variable2 est null.");
+} else {
+    console.log("La variable2 n'est pas null.");
+}
